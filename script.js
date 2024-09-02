823a6c64524c7926a51352a2a65e1fea86bc9889
Camryn Brown <108500312+camrynbrown@users.noreply.github.com>
2024-09-02 18:23:22 -0400

    Added the JavaScript.
    Local Database is not implemented.

---
+++ script.js
@@ -0,0 +1,26 @@
+const input = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container")
+
+function addTask() {
+    if (input.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = input.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+
+    input.value = "";
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
